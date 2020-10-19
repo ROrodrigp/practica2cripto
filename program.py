caa1df4e764c15fb11b9bde3e78f9bff9abb4033
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2020-10-19 17:29:12 -0500

    Ya esta separado en funciones

--- program.py
+++ program.py
@@ -1,60 +1,68 @@
 #!/bin/sh
 import fileinput
 #import codecs 
-S = list(range(0,256))
+
+
+
+
+#hex = codecs.encode(key, "hex")
+
+def KSA(key):
+	S = list(range(0,256))
+	keylength=len(key)
+	j = 0 
+
+	for i in range(256):
+		j = (j+S[i]+key[i%keylength])%256
+		S[i],S[j]= S[j],S[i]
+
+	return S
+
+
+def PRGA(S,mensaje):
+
+	KS = []
+	i = j = k = 0 
+
+	while (k < len(mensaje)):
+		i = (i+1)%256
+		j = (j+S[i])%256
+		S[i],S[j]= S[j],S[i]
+		t = (S[i]+S[j])%256
+		KS.append(S[t])
+		k+=1
+	return KS
+
+	#print(KS)
+def cifrado(key, mensaje):
+	S = KSA(key)
+	KS = PRGA(S, mensaje)
+	#print('S:',S)
+	#print('KS:',KS)
+	cifrado = []
+	for i in range(len(mensaje)):
+		C = KS[i] ^ mensaje[i]
+		cifrado.append(C)
+	
+	d=[]
+	e = 0 
+	y= ''
+	for i in cifrado:
+		#Estan en decimal 
+		d.append(i)
+		z = format(d[e],'02X')
+		y=y+z
+		e+=1
+	print(y)
 
 key= b"Wiki"
-M = b"pedia"
-keylength=len(key)
-#hex = codecs.encode(key, "hex")
-print(key[0])
-
-j = 0 
-for i in range(256):
-	#print('i: ',i)
-	#print('j:',j)
-	#print('\n',S)
-	j = (j+S[i]+key[i%keylength])%256
-	S[i],S[j]= S[j],S[i]
-
-KS = []
-
-i = j = k = 0 
-while (k < len(M)):
-	i = (i+1)%256
-	j = (j+S[i])%256
-	S[i],S[j]= S[j],S[i]
-	t = (S[i]+S[j])%256
-	KS.append(S[t])
-	k+=1
-print(KS)
-
-cifrado = []
-for i in range(len(M)):
-	C = KS[i] ^ M[i]
-	cifrado.append(C)
-
-print(cifrado)
-d=[]
-e = 0 
-y= ''
-for i in cifrado:
-	#Estan en decimal 
-	d.append(i)
-	z = format(d[e],'02X')
-	y=y+z
-	e+=1
-
-
-print(y)
+mensaje = b"pedia"
+cifrado(key,mensaje)
 
 
 
 
 
-x = str(d[0])
-x = x.strip("0x")
-print(x)
 
 
 
